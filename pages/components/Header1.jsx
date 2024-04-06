4178c8dab78f193a5395d53042629bc3d5a51bd6
Mansi Saraswat <mansisaraswat030@gmail.com>
2024-04-06 23:01:38 +0530

    navbar added

--- pages/components/Header1.jsx
+++ pages/components/Header1.jsx
@@ -1,22 +1,31 @@
-
 import Image from "next/image";
+import Link from "next/link"
 import Block from "./Block";
+import { block } from "../data/data";
+import { User } from 'lucide-react';
 const Header1 = () => {
   return (
-    <div className='flex justify-center items-center h-28 px-10'>
-<Image
-        src='/oyo.png'
+    <div className="flex justify-between border-b-2 border-gray-300 items-center h-20 px-10 ">
+      <Image
+        src="/oyo.png"
         alt="logo"
         width={200}
         height={200}
         className=" w-28 h-28 "
       />
-      <div>
-      <Block/>
+      <div className="flex h-full">
+      {block.map((item)=>(
+        <Block key={item.id} title={item.title} para={item.para} icon={item.icon} />
+      ))} 
       </div>
-      
+      <div className="flex items-center px-3 hover:bg-gray-100 h-full">
+          <User className="w-10 h-10 rounded-full mr-5 bg-gray-300"/>
+          <Link href={"/login"} className=" font-bold">
+              Login / Signup
+            </Link>
+          </div>
     </div>
-  )
-}
+  );
+};
 
 export default Header1;
---
+++ pages/data/data.js
@@ -0,0 +1,29 @@
+import { School,PhoneCall,BriefcaseBusiness,BadgeDollarSign } from 'lucide-react';
+export const block =[
+    {
+        id:1,
+        title:"Become a Member",
+        para:"Additional 10% off on stays",
+        icon:<BadgeDollarSign className=" w-8 h-8 mr-3"/>
+    },
+    {
+        id:2,
+        title:"OYO for Business",
+        para:"Trusted by 5000 Corporates",
+        icon:<BriefcaseBusiness className=" w-8 h-8  mr-3"/>
+    },
+    {
+        id:3,
+        title:"List your property",
+        para:"Start earning in 30 mins",
+        icon:<School className=" w-8 h-8 mr-3"/>
+    },
+    {
+        id:4,
+        title:"0124-6201611",
+        para:"Call us to Book now",
+        icon:<PhoneCall className=" w-8 h-8 mr-3"/>
+    },
+]
+
+
