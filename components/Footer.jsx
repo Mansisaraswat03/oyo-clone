1eafe2af6509c0104d5c3fa9b26d9c715c69fdfb
Mansi Saraswat <mansisaraswat030@gmail.com>
2024-05-08 12:25:17 +0530

    updated ui

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { Building2 } from 'lucide-react';
-
+import { Building2, FacebookIcon, InstagramIcon, LinkedinIcon, TwitterIcon, YoutubeIcon } from 'lucide-react';
+import { Facebook } from 'lucide-react';
+import { footer } from '@/lib/data/data';
 const Footer = () => {
   return (
     <div className="bg-gray-500 bottom-0">
@@ -16,13 +17,39 @@ const Footer = () => {
             <button className='bg-white text-black flex gap-1 rounded-md p-2'><Building2 />List your property</button>
           </div>
         </div>
-        <div className='py-5'>
-          <h2 className='text-white text-lg'>Download OYO app for exciting offers.</h2>
+        <hr/>
+        <div className='py-5 flex items-center gap-1 lg:gap-8'>
+        <div>
+          <h2 className='text-white text-sm lg:text-lg'>Download OYO app for exciting offers.</h2>
           <div className='flex flex-col lg:flex-row gap-4 mt-4'>
             <Link href="https://apps.apple.com/in/app/oyo-search-book-hotel-rooms/id988141624" target="_blank"><Image src="/apple.png" width={400} height={400} alt="logo" className='w-40 h-20' /></Link>
             <Link href="https://play.google.com/store/apps/details?id=com.oyo.consumer&hl=en" target="_blank"><Image src="/g-play.png" width={400} height={400} alt="logo" className='w-40 h-20' /></Link>
           </div>
         </div>
+        <div className="bg-white w-px h-80 lg:h-28 mx-2 lg:mx-8" />
+        <div className='grid grid-cols-1 text-sm lg:grid-cols-2 lg:text-lg gap-4'>
+        {footer.slice(0,7).map((user) => (
+          <a href={user.link} target='_blank'><p className='text-white mr-10 cursor-pointer hover:underline'>{user.name}</p></a>
+        ))}
+        </div>
+        <div className="bg-white w-px h-80 lg:h-28 mx-4 lg:mx-8" />
+        <div className='grid grid-cols-1 text-sm lg:grid-cols-2 lg:text-lg gap-4'>
+        {footer.slice(0,7).map((user) => (
+          <a href={user.link} target='_blank'><p className='text-white mr-10 cursor-pointer hover:underline'>{user.name}</p></a>
+        ))}
+        </div>
+        </div>
+        <hr/>
+       <div className='p-6 flex justify-between gap-4'>
+        <div className='flex items-center gap-2'>
+        <FacebookIcon className=" w-8 h-8  text-white "/>
+        <InstagramIcon className=" w-8 h-8 text-white "/>
+        <TwitterIcon className=" w-8 h-8 text-white "/>
+        <YoutubeIcon className=" w-8 h-8 text-white "/>
+        <LinkedinIcon className=" w-8 h-8 text-white "/>
+        </div>
+        <p className='text-white text-sm font-semibold'>2013-2022 © Oravel Stays Limited</p>
+       </div>
       </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,26 @@
 
 import Footer from "@/components/Footer";
 import { Header1 } from "@/components/Header1";
-import {Header2} from "@/components/Header2";
+import { Header2 } from "@/components/Header2";
 import Header3 from "@/components/Header3";
 import Header4 from "@/components/Header4";
+import Header5 from "@/components/Header5";
 import { Location } from "@/components/Location";
+import Head from "next/head";
 
 export default function Home() {
   return (
-   <div>
-   <Header1/>
-   <Header2/>
-   <Header3/>
-   <Header4/>
-   <Location/>
-   <Footer/>
-   </div>
+    <div>
+     <Head>
+        <title>OYO</title>
+      </Head>
+      <Header1 />
+      <Header2 />
+      <Header3 />
+      <Header4 />
+      <Header5/>
+      <Location />
+      <Footer />
+    </div>
   );
 }
