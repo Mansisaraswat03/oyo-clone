1785f72828bd47da101100247c020001ddcb3ff5
Mansi Saraswat <mansisaraswat030@gmail.com>
2024-05-30 14:34:12 +0530

    data modified

--- components/Header2.jsx
+++ components/Header2.jsx
@@ -6,7 +6,7 @@ export const Header2 = () => {
     <div>
       <div className='hidden md:flex gap-16  h-10 justify-center px-5 bg-gray-100'>
         {list.map((item,index)=>(
-            <div className="flex items-center px-1 hover:bg-white cursor-pointer" key={index}>{item.name} <ChevronDown/></div>
+            <Link href={item.link} className="flex items-center px-1 hover:bg-white cursor-pointer" key={index}>{item.name} <ChevronDown/></Link>
         ))}
         <div className="flex items-center hover:text-red-500 cursor-pointer" > All Cities</div>
       </div>
@@ -16,9 +16,9 @@ export const Header2 = () => {
             {item.name} <ChevronDown/>
           </Link>
         ))}
-        <div className="flex items-center hover:text-red-500 cursor-pointer">
+        <Link href="/hotels?city=" className="flex items-center hover:text-red-500 cursor-pointer">
           All Cities
-        </div>
+        </Link>
       </div>
     </div>
   )
